3c04036248c6018b2218854165eee1d2f73ce758
Mimi <trish21215@yahoo.com>
2023-07-05 18:45:31 -0400

    add entry points for zombie when they spawn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as PIXI from "pixi.js";
-//import Victor from "victor";
+import Victor from "victor";
 //import Matter from "matter-js";
 
 let canvasSize = 256;
@@ -20,6 +20,15 @@ square.tint = 0xea985d;
 
 app.stage.addChild(square);
 
+let zombieRadius = 16;
+const zombie = new PIXI.Graphics();
+let r = spawnPoint();
+zombie.position.set(r.x, r.y);
+zombie.beginFill(0xff0000, 1);
+zombie.drawCircle(0, 0, zombieRadius);
+zombie.endFill();
+app.stage.addChild(zombie)
+
 app.ticker.add((delta) => {
     const pointCursor = app.renderer.plugins.interaction.mouse.global;
     let movement = Math.atan2(pointCursor.y - square.position.y, pointCursor.x - square.position.x
@@ -27,3 +36,27 @@ app.ticker.add((delta) => {
         Math.PI / 2;
         square.rotation = movement;
 });
+
+function spawnPoint() {
+  let fourCorners = Math.floor(Math.random() * 4);
+  let spawn = new Victor(0, 0);
+switch(fourCorners) {
+  case 0: // this is the top of the screen
+    spawn.x = canvasSize * Math.random();
+    break;
+  case 1: // this is the right side of the screen
+    spawn.x = canvasSize;
+    spawn.y = canvasSize * Math.random();
+      break;
+  case 2: // this is the bottom of the screen
+    spawn.x = canvasSize * Math.random();
+    spawn.y = canvasSize;
+    break;
+  default:
+    // this is the left side of the screen
+    spawn.x = 0;
+    spawn.y = canvasSize * Math.random();
+    break;
+}
+return spawn;
+}
